4e90ebc0045afb82b1f2bf0c120caccdc27cc01b
RomanMelnyk909 <45914004+RomanMelnyk909@users.noreply.github.com>
2021-12-16 14:10:32 +0200

    edit basic pagination

--- src/comonents/Home/index.tsx
+++ src/comonents/Home/index.tsx
@@ -2,13 +2,14 @@ import CarsList from "../CarsList";
 import CarSearch from "../CarsList/CarSearch/CarSearch";
 
 const HomePage = () => {
-    
-    return (
-        <>
-        <h1 className="text-center">Список автомобілів</h1>
-        <CarSearch />
-        <CarsList />
-        </>
-    );
-}
+  return (
+    <>
+  
+
+      <h1 className="text-center">Список автомобілів</h1>
+      <CarSearch />
+      <CarsList />
+    </>
+  );
+};
 export default HomePage;
