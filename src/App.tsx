910e44cd2113ab5bad5839deb706bf16f3dcf419
OleksandrKyrylyuk <83026682+OleksandrKyrylyuk@users.noreply.github.com>
2021-12-21 21:45:51 +0200

    addLazyLoading

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
+//Basic imports
+import React, { Suspense, useEffect } from "react";
 import { Route, Routes } from "react-router";
+import { useTypedSelector } from "./hooks/useTypedSelector";
+import { useActions } from "./hooks/useActions";
+
+//ImportModules
 import "./App.css";
+import "../node_modules/font-awesome/css/font-awesome.css";
+
+//Impor components
 import Home from "./components/Home";
-import Register from "./components/auth/Register/index";
 import DefaultLayout from "./components/containers/DefaultLayout";
 import AddNewCar from "./components/AddNewCar";
 import LoginPage from "./components/auth/Login";
 import ProfilePage from "./components/Profile";
-import "../node_modules/font-awesome/css/font-awesome.css";
-import { useTypedSelector } from "./hooks/useTypedSelector";
 import Cart from "./components/Cart/Cart";
 import UserDetailPage from "./components/Users/UserPage";
 import NoMatch from "./components/NoMatch";
 import EditPage from "./components/Users/EditPage";
-import { useActions } from "./hooks/useActions";
-import { useEffect } from "react";
 import CreatePage from "./components/Users/CreatePage";
 import UserSearch from "./components/Users/UserSearch";
 import CarsListAdmin from "./components/CarsList/CarListAdmin/CarListAdmin";
@@ -22,6 +26,8 @@ import EditCarPage from "./components/CarsList/CarListAdmin/EditCarPage";
 import RecoverPassword from "./components/auth/recoverPassword";
 import SendEmail from "./components/auth/recoverPassword/recoverSuccess";
 import ResetPassword from "./components/auth/recoverPassword/resetPassword";
+//Import lazyLoading
+const Register = React.lazy(() => import("./components/auth/Register/index"));
 
 function App() {
   const { cartIsShow } = useTypedSelector((store) => store.cart);
@@ -37,17 +43,38 @@ function App() {
       <Routes>
         <Route path="/" element={<DefaultLayout />}>
           <Route index element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/products/add" element={<AddNewCar />} />
-          <Route path="/login" element={<LoginPage />} />
+          {/* AuthRoutes */}
+          <Route
+            path="/register"
+            element={
+              <Suspense fallback={null}>
+                <Register />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <Suspense fallback={null}>
+                <LoginPage />
+              </Suspense>
+            }
+          />
+
+          {/* RcoverPasswordRoutes */}
           <Route path="/recoverPassword" element={<RecoverPassword />} />
           <Route path="/recoverPassword/sendEmail" element={<SendEmail />} />
           <Route path="/resetpassword" element={<ResetPassword />} />
+
+          {/* UserCrud Routes */}
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="/users" element={<UserSearch />} />
           <Route path="/users/:id" element={<UserDetailPage />} />
           <Route path="/users/edit/:id" element={<EditPage />} />
           <Route path="/users/create" element={<CreatePage />} />
+
+          {/* Products Routes */}
+          <Route path="/products/add" element={<AddNewCar />} />
           <Route path="/cars" element={<CarsListAdmin />} />
           <Route path="/cars/:id" element={<CarPage />} />
           <Route path="/cars/edit/:id" element={<EditCarPage />} />
