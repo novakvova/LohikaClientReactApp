9cc5894cd445ed1016f19483b75df0d92256d737
Katherine1110 <k.mihalochkina@gmail.com>
2022-01-26 15:49:47 +0200

    add styles
    Added responsive styles for categories home page

--- src/components/Categories/CategoriesHomePage/CategoriesListHomePage.tsx
+++ src/components/Categories/CategoriesHomePage/CategoriesListHomePage.tsx
@@ -3,25 +3,27 @@ import { useActions } from '../../../hooks/useActions';
 import { useTypedSelector } from '../../../hooks/useTypedSelector';
 import CategoryItemHomePage from '../CategoriesHomePage/CategoryItemHomePage';
 import { CategoryInfo } from '../types';
+import './style.css'
 
-const CategoriesListHomePage = () =>{
-    
-    const { categories } = useTypedSelector( store => store.categoryCrud)
-    const { getSearchCategoryResult} = useActions();
-    
-    useEffect(() => {
-      getSearchCategoryResult({});
-    }, [getSearchCategoryResult]);
-  
-      return (
-        <div className="container d-flex justify-content-around mb-5">
-            {categories.map((categoryItem: CategoryInfo, idx) => (
-                <CategoryItemHomePage key={idx} categoryItem={categoryItem} />
-            ))}
-            
-          
+
+const CategoriesListHomePage = () => {
+  const { categories } = useTypedSelector((store) => store.categoryCrud);
+  const { getSearchCategoryResult } = useActions();
+
+  useEffect(() => {
+    getSearchCategoryResult({});
+  }, [getSearchCategoryResult]);
+
+  return (
+    <>
+      <h2 className='d-flex justify-content-center mt-5'>Категорії товарів</h2>
+      <div className="d-flex justify-content-around mb-5 mt-5 responsiveContainer">
+        {categories.map((categoryItem: CategoryInfo, idx) => (
+          <CategoryItemHomePage key={idx} categoryItem={categoryItem} />
+        ))}
       </div>
-    );
-}
+    </>
+  );
+};
 
 export default CategoriesListHomePage;
--- src/components/Categories/CategoriesHomePage/CategoryItemHomePage.tsx
+++ src/components/Categories/CategoriesHomePage/CategoryItemHomePage.tsx
@@ -1,26 +1,38 @@
 import { v4 as uuid } from 'uuid';
 import { CategoryInfo } from '../types';
 import { Link } from 'react-router-dom';
+import { Button } from 'primereact/button';
+import { Card } from 'primereact/card';
+import './style.css'
 
 interface Props {
   categoryItem: CategoryInfo;
 }
 
 const CategoryItemHomePage: React.FC<Props> = ({ categoryItem: { id, title, image } }) => {
+  const header = (
+    <img 
+    height="150"
+    width='200'
+    alt={title} 
+    src={`https://vovalohika.tk/images/300_${image}?t=${uuid()}`} 
+    onError={(event: React.SyntheticEvent<HTMLImageElement, Event>) =>
+      ((event.target as HTMLImageElement).src =
+        "https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png")
+    } />
+);
 
   return (
-    <Link to={`/adminPanel/categories/get/${id}`}>
-      <div className="card text-dark bg-light mb-3">
-        <div className="card-body">
-          <img
-            className="d-block mb-3 w-300"
-            src={`https://vovalohika.tk/images/300_${image}?t=${uuid()}`}
-            alt="Avatar"
-          />
-          <div className="text-dark">{title}</div>
-        </div>
-      </div>
-    </Link>
+    <div>
+      <Card className='element'
+        title={title}
+        style={{ 
+          width: '17em', 
+          background: "fixed",
+          color: "#2a323d" }}
+        header={header}>
+      </Card>
+    </div>
   );
 };
 
