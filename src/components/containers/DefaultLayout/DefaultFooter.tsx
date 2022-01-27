a70bf7f4ca008a4101821e556cb43ae60f4ff082
OleksandrKyrylyuk <83026682+OleksandrKyrylyuk@users.noreply.github.com>
2022-01-27 17:16:03 +0200

    finishNewsCrud

--- src/components/containers/DefaultLayout/DefaultFooter.tsx
+++ src/components/containers/DefaultLayout/DefaultFooter.tsx
@@ -1,12 +1,13 @@
-import { ReactComponent as Icon } from './DefaultLayout/icons8.svg';
+import { Link } from 'react-router-dom';
+
 
 const DefaultFooter = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
-        <a className="navbar-brand" href="#">
+        <Link to="/" className="navbar-brand">
           LOGO
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -20,34 +21,34 @@ const DefaultFooter = () => {
         <div className="collapse navbar-collapse" id="navbarText">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
+              <Link to="/" className="nav-link active" aria-current="page" >
                 Home
-              </a>
+              </Link >
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <Link to="/" className="nav-link" >
                 Features
-              </a>
+              </Link >
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <Link to="/" className="nav-link" >
                 Pricing
-              </a>
+              </Link >
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <Link to="/" className="nav-link" >
                 FaceBook
-              </a>
+              </Link >
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <Link to="/" className="nav-link" >
                 Instagram
-              </a>
+              </Link >
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <Link to="/" className="nav-link" >
                 Google
-              </a>
+              </Link>
             </li>
           </ul>   
         </div>
